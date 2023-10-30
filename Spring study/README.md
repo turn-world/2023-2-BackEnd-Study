92132ad17392526a2b7106c7bb07af96a0b56d84
조용준 <whdydwns0604@gmail.com>
2023-10-30 23:03:29 +0900

    오류 해결

--- Spring study/README.md
+++
@@ -1,3 +0,0 @@
-인프런을 통한 기초 스프링 부트 JAVA11
-단축키 목록
-1. getter setter 단축키 alt+insert
--- Spring study/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ Spring study/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package main.java.hello.hellospring;
+package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
